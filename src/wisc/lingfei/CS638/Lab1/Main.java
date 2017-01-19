9b517f00c9d6de4f9fa6717c0ecad1674c0311d5
Lingfei Li <cslilingfei@outlook.com>
2017-01-18 21:18:43 -0600

    First commit

---
+++ src/wisc/lingfei/CS638/Lab1/Main.java
@@ -0,0 +1,14 @@
+package wisc.lingfei.CS638.Lab1;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Hello CS638!");
+
+
+
+    }
+}
